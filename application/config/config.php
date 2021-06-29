32298b2fe49b20d61e3a0bb5d26c0e687e3e548f
X4D0 <46711970+X4D0@users.noreply.github.com>
2021-06-29 11:57:11 +0700

    Change Base URL

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/layanan_umum/';
+$config['base_url'] = 'http://localhost/PT-INTI-Layanan-Umum/';
 
 /*
 |--------------------------------------------------------------------------
