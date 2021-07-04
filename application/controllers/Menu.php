2033b7b3f10ce1c1c077a12aa13c5cca8083b6ed
rizkan03 <61906974+rizkan03@users.noreply.github.com>
2021-07-04 20:20:42 +0700

    tambah func buat tree menu

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -103,4 +103,138 @@ class Menu extends CI_Controller
     $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Sub Menu Dihapus!</div>');
     redirect('menu/submenu');
   }
+	//KELOLA TREE MENU 
+
+  //SSMenu
+  public function ssmenu()
+  {
+    $data['title'] = 'SSubmenu Management';
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $data['menu'] = $this->db->get('user_menu')->result_array();
+    $this->load->model('Menu_model', 'menu');
+
+    $data['ssubMenu'] = $this->menu->getSSubMenu();
+    $data['menu'] = $this->db->get('user_menu')->result_array();
+
+    $this->form_validation->set_rules('title', 'Title', 'required');
+    $this->form_validation->set_rules('menu_id', 'Menu', 'required');
+    $this->form_validation->set_rules('url', 'URL', 'required');
+    $this->form_validation->set_rules('icon', 'icon', 'required');
+
+    if ($this->form_validation->run() == false) {
+      $this->load->view('templates/header', $data);
+      $this->load->view('templates/sidebar', $data);
+      $this->load->view('templates/topbar', $data);
+      $this->load->view('menu/ssmenu', $data);
+      $this->load->view('templates/footer');
+    } else {
+      $data = [
+        'title' => $this->input->post('title'),
+        'menu_id' => $this->input->post('menu_id'),
+        'url' => $this->input->post('url'),
+        'icon' => $this->input->post('icon'),
+        'is_active' => $this->input->post('is_active')
+      ];
+      $this->db->insert('user_ss_menu', $data);
+      $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">New Submenu added!</div>');
+      redirect('menu/ssmenu');
+    }
+  }
+
+  public function editssmenu($id)
+  {
+    $data = [
+      'title' => $this->input->post('title'),
+      'menu_id' => $this->input->post('menu'),
+      'url' => $this->input->post('url'),
+      'icon' => $this->input->post('icon'),
+      'is_active' => $this->input->post('aktif')
+    ];
+    $nama = $this->input->post('menu');
+    $this->db->where('id', $id);
+    $this->db->update('user_ss_menu', $data);
+    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Sub Menu Diedit!</div>');
+    redirect('menu/ssmenu');
+  }
+
+  public function delssmenu($id)
+  {
+    $this->db->where('id', $id);
+    $this->db->delete('user_ss_menu');
+    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Sub Menu Dihapus!</div>');
+    redirect('menu/ssmenu');
+  }
+
+  //TreeMenu
+  public function treemenu()
+  {
+    $data['title'] = 'Treemenu Management';
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $data['menu'] = $this->db->get('user_menu')->result_array();
+    $this->load->model('Menu_model', 'menu');
+
+    $data['treeMenu'] = $this->menu->getTreeMenu();
+    $data['menu'] = $this->db->get('user_menu')->result_array();
+    $data['ssmenu'] = $this->db->get('user_ss_menu')->result_array();
+
+    $this->form_validation->set_rules('title', 'Title', 'required');
+    $this->form_validation->set_rules('menu_id', 'Menu', 'required');
+    $this->form_validation->set_rules('url', 'URL', 'required');
+    $this->form_validation->set_rules('icon', 'icon', 'required');
+
+    if ($this->form_validation->run() == false) {
+      $this->load->view('templates/header', $data);
+      $this->load->view('templates/sidebar', $data);
+      $this->load->view('templates/topbar', $data);
+      $this->load->view('menu/treemenu', $data);
+      $this->load->view('templates/footer');
+    } else {
+      $data = [
+        'title' => $this->input->post('title'),
+        'menu_id' => $this->input->post('menu_id'),
+        'menu_tree' => $this->input->post('ssmenu_id'),
+        'url' => $this->input->post('url'),
+        'icon' => $this->input->post('icon'),
+        'is_active' => $this->input->post('is_active')
+      ];
+
+      $this->db->insert('user_tree_menu', $data);
+      $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Tree menu berhasil ditambahkan!</div>');
+      redirect('menu/treemenu');
+    }
+  }
+
+  public function get_idmenu()
+  {
+    $this->load->model('Menu_model', 'menu');
+    $id = $this->input->post('menu_id');
+    $data = $this->menu->getSSNameMenu($id);
+
+    echo json_encode($data);
+  }
+
+  public function edittreemenu($id)
+  {
+    $data = [
+      'title' => $this->input->post('title'),
+      'menu_id' => $this->input->post('menu_id'),
+      'menu_tree' => $this->input->post('ss_menu_id'),
+      'url' => $this->input->post('url'),
+      'icon' => $this->input->post('icon'),
+      'is_active' => $this->input->post('aktif')
+    ];
+    $nama = $this->input->post('menu');
+    $this->db->where('id', $id);
+    $this->db->update('user_tree_menu', $data);
+    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Tree menu berhasil diedit!</div>');
+    redirect('menu/treemenu');
+  }
+
+  public function deltreemenu($id)
+  {
+    $this->db->where('id', $id);
+    $this->db->delete('user_tree_menu');
+    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Tree menu berhasil dihapus!</div>');
+    redirect('menu/treemenu');
+  }
 }
